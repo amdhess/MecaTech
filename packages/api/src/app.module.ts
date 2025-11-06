822616dced0d3b517cc076de54406131dccb31f1
amandaO2hess <amandacristinahess@gmail.com>
2025-11-06 00:29:34 -0300

    chore(api): register all feature modules in AppModule

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ClientModule } from './client/client.module';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { ServiceModule } from './service/service.module';
+import { OrderModule } from './order/order.module';
+import { PartModule } from './part/part.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ClientModule,
+    VehicleModule,
+    ServiceModule,
+    OrderModule,
+    PartModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
